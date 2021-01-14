ba16218690e8e804b884f4143ec7806bdf27cff2
dawidurbanski4 <urbdadylbod@gmail.com>
2021-01-14 12:36:41 +0000

    solved2

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -7,23 +7,35 @@
 // Zad1
 // tutaj definicja klasy Tagliatelle
 
-/*
-class Makaron 
+
+class Makaron
 {
-  public:
-  double ileMaki(unsigned) const =0;
-  Makaron* gotujMakaron(std::string a)
-  if(a.front==a.back){
-    return new Tagliatelle(3.14,0.42,0.1);
-  }else{
-    return new Penne;
-  }
+public:
+    virtual double ileMaki(unsigned) const = 0;
+    static Makaron* gotujMakaron(const std::string& nazwa);
+};
 
-  ~Makar
+class Tagliatelle : public Makaron
+{
+public:
+    Tagliatelle() { this->W = 0.5; this->R = 0.5; this->L = 0.5; };
+    Tagliatelle(double a, double b, double c) : W(a), R(b), L(c) {};
 
-}
-*/
-class Tagliatelle //: public Makaron
+    double ileMaki(unsigned P) const
+    {
+        return P * this->L*this->W*(1. - this->R)*this->C;
+    }
+
+private:
+    double W;
+    double R;
+    double L;
+    const static double C;
+
+};
+
+
+class Tagliatelle : public Makaron
 {
   public:
   Tagliatelle(): L(0.5), W(0.5), R(0.5) {}
